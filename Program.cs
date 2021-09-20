e3855c175a742b646f74f4e01e82b36b6144a9c0
AishaCod3s <90952566+AishaCod3s@users.noreply.github.com>
2021-09-20 13:21:48 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Subtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 0;
+            int secondNumber = 0;
+            int result = 0;
+
+            Console.Write("Please enter your first number; ");
+            firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter your second number; ");
+            secondNumber = int.Parse(Console.ReadLine());
+
+            result = firstNumber - secondNumber;
+            Console.WriteLine("The answer is; " + result);
+        }
+    }
+}
