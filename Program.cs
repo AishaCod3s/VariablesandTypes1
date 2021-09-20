d7a444e799c7be9b69b5a8e171bed43d6f825ec7
AishaCod3s <90952566+AishaCod3s@users.noreply.github.com>
2021-09-20 13:28:30 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 0;
+            int secondNumber = 0;
+            int result = 0;
+
+            Console.Write("Please enter your first number; ");
+            firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter your second number; ");
+            secondNumber = int.Parse(Console.ReadLine());
+
+            result = firstNumber / secondNumber;
+            Console.WriteLine("The answer is; " + result);
+
+        }
+    }
+}
